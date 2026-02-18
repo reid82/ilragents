66480430fd103f7562ccc3023913f7901acc2d98
reid82 <reid@reidbates.com>
2026-02-18 15:09:01 +1100

    feat: add calculator barrel export

---
+++ packages/pipeline/src/pipeline/calculators/index.ts
@@ -0,0 +1,11 @@
+export { calculateFISO } from './fiso-calculator';
+export type { FISOInput, FISOOutput } from './fiso-calculator';
+
+export { calculateCashflow } from './cashflow-calculator';
+export type { CashflowInput, CashflowOutput } from './cashflow-calculator';
+
+export { runSensitivityAnalysis } from './sensitivity-engine';
+export type { SensitivityOutput } from './sensitivity-engine';
+
+export { calculateCapacity } from './capacity-calculator';
+export type { CapacityInput, CapacityOutput } from './capacity-calculator';
